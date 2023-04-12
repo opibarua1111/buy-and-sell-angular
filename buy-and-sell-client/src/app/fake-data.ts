99697db1a4814ea749551b40cec0856b74dc3ccd
opi <64263323+opibarua1111@users.noreply.github.com>
2023-04-12 11:39:41 +0600

    buy and sell client

---
+++ buy-and-sell-client/src/app/fake-data.ts
@@ -0,0 +1,22 @@
+import { Listing } from './types';
+
+export const fakeListings: Listing[] = [
+  {
+    id: '123',
+    name: 'Old Boat',
+    description: 'A very old Boat. Bargain price',
+    price: 50,
+  },
+  {
+    id: '345',
+    name: 'Computer',
+    description: 'From the 1990s, a classic!',
+    price: 50,
+  },
+  {
+    id: '456',
+    name: 'Basketball hoop',
+    description: 'Good conduction, free delivery',
+    price: 50,
+  },
+];
---
+++ buy-and-sell-client/src/app/listing-data-form/listing-data-form.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { Listing } from '../types';
+
+@Component({
+  selector: 'app-listing-data-form',
+  templateUrl: './listing-data-form.component.html',
+  styleUrls: ['./listing-data-form.component.css'],
+})
+export class ListingDataFormComponent implements OnInit {
+  @Input() buttonText;
+  @Input() currentName = '';
+  @Input() currentDescription = '';
+  @Input() currentPrice: number;
+
+  name: string = '';
+  description: string = '';
+  price: number = 0;
+
+  @Output() onSubmit = new EventEmitter<Listing>();
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    (this.name = this.currentName),
+      (this.description = this.currentDescription),
+      (this.price = this.currentPrice);
+  }
+
+  onButtonClicked(): void {
+    this.onSubmit.emit({
+      id: null,
+      name: this.name,
+      description: this.description,
+      price: Number(this.price),
+    });
+  }
+}
